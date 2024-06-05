b257f58ca6a38c74d16f1dff90874535b1fd0fb4
«Mariia <raviolioli@icloud.com>
2024-06-05 19:32:28 +1000

    commit

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -18,17 +18,21 @@ import platform
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ["what", "does", "this", "line", "do", "?"]
-
+# I think this will return every item from the list 
 for word in some_words:
     print(word)
-
+# Actually return items from the list
+# Does the same as the upper one because x is similar to words and python does not mind how you call an item
 for x in some_words:
     print(x)
-
+# Does actually the same and return items from the list
+# Prints the list 
 print(some_words)
-
+# Really have returned the list 
+# If the quantity of items in the list is bigger than 3, will tell that the list contains more than 3 words. If false, will return nothing.
 if len(some_words) > 3:
     print("some_words contains more than 3 words")
+    # Does as expected.
 
 
 def usefulFunction():
@@ -37,6 +41,7 @@ def usefulFunction():
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
+    # Return a list contains something
     print(platform.uname())
 
 
