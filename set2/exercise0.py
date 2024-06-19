ad24562294edb057fbc7bedea62804da84ffb8f9
«Mariia <raviolioli@icloud.com>
2024-06-19 18:43:07 +1000

    a

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -50,12 +50,12 @@ def add_5(a_number):
 
 
 def adder(a_number, another_number):
+    return a_number + another_number
     """Add two numbers.
 
     Same as above, but with any two numbers.
     """
-    the_answer = None
-    return the_answer
+    #.
 
 
 def shout(a_string):
@@ -67,8 +67,8 @@ def shout(a_string):
     or in any of the million places that google will give you.
     "python make a string uppercase" is a good starting search query.
     HINT: there are a few things with upper case in their description, but
-          they all do different things. You'll need to actually read the
-          docs to find out which one you actually need.
+        they all do different things. You'll need to actually read the
+        docs to find out which one you actually need.
     """
     the_answer = a_string.upper()
     return the_answer
@@ -94,10 +94,10 @@ def shout_with_a_number(a_string, a_number):
     'HELLO 42'
 
     HINT: Lookup how to cast a_number to a string or lookup how to use
-          string formatting in python.
-          There are a few ways to do this, so if you're looking for a
-          challenge, see if you can make the test pass with at least two ways
-          of doing the same job.
+        string formatting in python.
+        There are a few ways to do this, so if you're looking for a
+        challenge, see if you can make the test pass with at least two ways
+        of doing the same job.
     """
     the_answer = a_string.upper()
     full_answer = a_number
@@ -112,15 +112,15 @@ if __name__ == "__main__":
         from helper import minitest
         print(
             """
-              This section does a quick test on your results and prints them nicely
-              It's NOT the official tests, they are in tests.py as usual.
-              Add to these tests if you want, give them arguments etc. to make sure that your
-              code is robust to the situations that you'll see in action.
+            This section does a quick test on your results and prints them nicely
+            It's NOT the official tests, they are in tests.py as usual.
+            Add to these tests if you want, give them arguments etc. to make sure that your
+            code is robust to the situations that you'll see in action.
     
-              the format is: minitest(function_name, [list, of, arguments], expected_result)
+            the format is: minitest(function_name, [list, of, arguments], expected_result)
     
-              REMEMBER: these aren't the tests that you submit, these are just
-              there to keep you sane."""
+            REMEMBER: these aren't the tests that you submit, these are just
+            there to keep you sane."""
         )
     
         minitest(add_1, [1], 2)
