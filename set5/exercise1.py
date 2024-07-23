a217439caebb20890ef12dc0fe03e7bba963fd94
«Mariia <raviolioli@icloud.com>
2024-07-23 13:38:25 +1000

    ?????????????

--- set5/exercise1.py
+++ set5/exercise1.py
@@ -30,32 +30,21 @@ def wordy_pyramid():
         "give_me_a_word?wordlength={length}"
     )
     pyramid_list = []
-    for i in range(3, 21, 2):
-        url = baseURL.format(length=i)
+    def get_a_word_of_length_n(length):
+        url = baseURL.format(length=length)
         r = requests.get(url)
-        if r.status_code is 200:
+        if r.status_code == 200:
             message = r.text
             pyramid_list.append(message)
         else:
-            print("failed a request", r.status_code, i)
-    for i in range(20, 3, -2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+            print("failed a request", r.status_code, length)
 
+    def list_of_words_with_lengths(length,list_of_lengths):
+        for length in range(list_of_lengths):
+            get_a_word_of_length_n(length)
+    
     return pyramid_list
 
-
-def get_a_word_of_length_n(length):
-    pass
-
-
-def list_of_words_with_lengths(list_of_lengths):
-    pass
 
 
 if __name__ == "__main__":
